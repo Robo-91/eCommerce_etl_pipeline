03b864e6a90cd1458fa49c3eba0d05db40b2f44e
Robert Palmer <r91palmer@gmail.com>
2026-07-16 09:31:25 -0500

    added ingestion logic for sellers csv data into staging table

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from src.ingestion.ingest_order_reviews_csv import ingest_order_reviews
 from src.ingestion.ingest_orders_csv import ingest_orders
 from src.ingestion.ingest_product_category_name_translation_csv import ingest_product_category_name_translation
 from src.ingestion.ingest_products_csv import ingest_products
+from src.ingestion.ingest_sellers_csv import ingest_sellers
 
 def ingestion_job() :
     ingest_order_reviews()
     ingest_orders()
     ingest_product_category_name_translation()
     ingest_products()
+    ingest_sellers()
 
 ingestion_job()
