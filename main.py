4dc30a4cd99370c314e9e93ca063d349762f9d99
Robert Palmer <r91palmer@gmail.com>
2026-07-16 09:23:23 -0500

    added ingestion logic for products into staging table

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.ingestion.ingest_order_reviews_csv import ingest_order_reviews
 from src.ingestion.ingest_orders_csv import ingest_orders
 from src.ingestion.ingest_product_category_name_translation_csv import ingest_product_category_name_translation
+from src.ingestion.ingest_products_csv import ingest_products
 
 def ingestion_job() :
     ingest_order_reviews()
     ingest_orders()
     ingest_product_category_name_translation()
+    ingest_products()
 
 ingestion_job()
---
+++ src/ingestion/ingest_products_csv.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+from datetime import date
+import yaml
+from src.util.config_load import load_filepath
+
+def ingest_products():
+
+    # Access YAML file for required configurations
+    yaml_config = load_filepath()
+
+    with open(yaml_config) as file:
+        config = yaml.safe_load(file)
+
+    # Connect to data sources
+    # Read csv data source
+    products_df = pd.read_csv(config['csv']['products'])
+
+    # Connect to SQL Server
+    ## Create required variables for SSMS connection
+    server = config['database']['server']
+    database = config['database']['database']
+    driver = config['database']['driver']
+    database_conn = f'mssql+pyodbc://@{server}/{database}?driver={driver}'
+
+    ## Create connection via SQL alchemy
+    engine = create_engine(database_conn)
+    con = engine.connect()
+
+    # Truncate Staging Table
+    truncate_query = 'TRUNCATE TABLE Products_Staging'
+    con.execute(text(truncate_query))
+
+    # load data
+    products_df.to_sql(
+        'Products_Staging',
+        con = con,
+        if_exists = 'append',
+        index = False
+    )
+
+    # log results
+    log_data = {'row_count': [len(products_df)], 'table_name': ['Products_Staging'], 'load_date': [date.today()]}
+    log_data_df = pd.DataFrame(log_data)
+    log_data_df.to_sql(
+        name = 'LoadStagingTablesLog',
+        con = con,
+        if_exists = 'append',
+        index = False
+    )
+
+    # Commit changes and close connection
+    con.commit()
+    con.close()
